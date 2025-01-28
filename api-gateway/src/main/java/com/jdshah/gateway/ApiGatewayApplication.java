33e39a76201ec1c0f98fd62b86b18cc0de4f22db
Jainam Shah <jainam.shah2@cygnet.one>
2025-01-28 15:52:31 +0530

    Api Gateway routes configured

--- api-gateway/src/main/java/com/jdshah/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/jdshah/gateway/ApiGatewayApplication.java
@@ -2,9 +2,7 @@ package com.jdshah.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 public class ApiGatewayApplication {
 
@@ -12,21 +10,4 @@ public class ApiGatewayApplication {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
-//	@Bean
-//	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-//		return builder.routes()
-//				.route(p -> p
-//						.path("/identity/**")
-//						.uri("lb://IDENTITY-SERVICE"))
-//				.route(p -> p
-//						.path("/products", "/products/**")
-//						.uri("lb://PRODUCT-SERVICE"))
-//				.route(p -> p
-//						.path("/orders/**")
-//						.uri("lb://ORDER-SERVICE"))
-//				.route(p -> p
-//						.path("/inventory/**")
-//						.uri("lb://INVENTORY-SERVICE"))
-//				.build();
-//	}
 }
