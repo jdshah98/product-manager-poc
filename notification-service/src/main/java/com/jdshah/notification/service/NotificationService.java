51b897115bedcb385173b42010586f3f41322dbb
Jainam Shah <jainam.shah2@cygnet.one>
2025-01-29 13:32:19 +0530

    Added Notification Service and implemented kafka for Distributed communincation between Order and Notification Service

---
+++ notification-service/src/main/java/com/jdshah/notification/service/NotificationService.java
@@ -0,0 +1,45 @@
+package com.jdshah.notification.service;
+
+import com.jdshah.order.event.OrderPlacedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class NotificationService {
+    private final JavaMailSender javaMailSender;
+
+    @KafkaListener(groupId = "notificationService", topics = "order-placed")
+    public void listen(OrderPlacedEvent orderPlacedEvent) {
+        log.info("Got message from order-placed topic {}", orderPlacedEvent);
+
+        MimeMessagePreparator mimeMessagePreparator = mimeMessage -> {
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
+            mimeMessageHelper.setFrom("springshop@email.com");
+            mimeMessageHelper.setTo(orderPlacedEvent.getEmail().toString());
+            mimeMessageHelper.setSubject(String.format("Your order with orderNumber %s is placed successfully", orderPlacedEvent.getOrderNumber()));
+            mimeMessageHelper.setText(String.format("""
+                        Hi
+                    
+                        Your order with order number %s is now placed successfully
+                    
+                        Best Regards,
+                        Spring Shop
+                    """, orderPlacedEvent.getOrderNumber()));
+        };
+        try {
+            javaMailSender.send(mimeMessagePreparator);
+            log.info("Order Notification Email Sent");
+        } catch (MailException e) {
+            log.error("Exception occurred when sending mail");
+            throw new RuntimeException("Exception occurred when sending mail to " + orderPlacedEvent.getEmail(), e);
+        }
+    }
+}
--- order-service/src/main/java/com/jdshah/order/service/OrderService.java
+++ order-service/src/main/java/com/jdshah/order/service/OrderService.java
@@ -2,10 +2,12 @@ package com.jdshah.order.service;
 
 import com.jdshah.order.client.InventoryClient;
 import com.jdshah.order.dto.OrderRequest;
+import com.jdshah.order.event.OrderPlacedEvent;
 import com.jdshah.order.model.Order;
 import com.jdshah.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -14,6 +16,7 @@ import org.springframework.stereotype.Service;
 public class OrderService {
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest) {
         var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
@@ -28,6 +31,11 @@ public class OrderService {
 
             orderRepository.save(order);
             log.info("Order created successfully");
+
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent(order.getOrderNumber(), orderRequest.userDetails().email());
+            log.info("Start - sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
+            kafkaTemplate.send("order-placed", orderPlacedEvent);
+            log.info("End - sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
         } else {
             throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is not in stock");
         }
